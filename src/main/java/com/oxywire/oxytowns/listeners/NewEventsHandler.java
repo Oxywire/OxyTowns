69aae4a41a006c948088d4f1c78140fd607c3a27
SirSalad <sirsalad.mc@gmail.com>
2024-05-06 00:44:11 -0400

    fix: add decorated_pot to interact set

--- src/main/java/com/oxywire/oxytowns/listeners/NewEventsHandler.java
+++ src/main/java/com/oxywire/oxytowns/listeners/NewEventsHandler.java
@@ -89,7 +89,7 @@ public class NewEventsHandler implements Listener {
     private static final Set<Material> REDSTONE_MATERIALS = Sets.newHashSet(Material.DISPENSER, Material.DROPPER, Material.HOPPER, Material.DAYLIGHT_DETECTOR,
         Material.REPEATER, Material.COMPARATOR, Material.NOTE_BLOCK, Material.JUKEBOX);
     private static final Set<Material> DOOR_MATERIALS = EnumSet.noneOf(Material.class);
-    private static final Set<Material> INTERACT_SETS = EnumSet.of(Material.PUMPKIN, Material.CAKE, Material.CAVE_VINES_PLANT, Material.CAVE_VINES, Material.SWEET_BERRY_BUSH, Material.RESPAWN_ANCHOR);
+    private static final Set<Material> INTERACT_SETS = EnumSet.of(Material.PUMPKIN, Material.CAKE, Material.CAVE_VINES_PLANT, Material.CAVE_VINES, Material.SWEET_BERRY_BUSH, Material.RESPAWN_ANCHOR, Material.DECORATED_POT);
 
     static {
         CHEST_MATERIALS.addAll(Tag.SHULKER_BOXES.getValues());
