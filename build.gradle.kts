0d8bcb1dc647eca4fa12a0e3946484e255515973
SirSalad <sirsalad.mc@gmail.com>
2024-09-17 21:57:21 -0400

    chore(deps): bump placeholderapi to 2.11.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
-    compileOnly("me.clip:placeholderapi:2.11.3")
+    compileOnly("me.clip:placeholderapi:2.11.6")
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.9")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
     compileOnly("xyz.jpenilla:squaremap-api:1.1.15")
