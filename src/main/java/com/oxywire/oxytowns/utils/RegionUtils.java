a6c4f3c427989402ed04521f691a9e1f2463e9aa
SirSalad <sirsalad.mc@gmail.com>
2025-03-04 15:14:51 -0500

    fix: being able to claim chunks that contain regions

--- src/main/java/com/oxywire/oxytowns/utils/RegionUtils.java
+++ src/main/java/com/oxywire/oxytowns/utils/RegionUtils.java
@@ -2,12 +2,16 @@ package com.oxywire.oxytowns.utils;
 
 import com.oxywire.oxytowns.config.Config;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import lombok.experimental.UtilityClass;
 import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
 import org.bukkit.Location;
+import org.bukkit.World;
 
 import java.util.Set;
 
@@ -23,10 +27,15 @@ public final class RegionUtils {
     public boolean isInRegion(final Location location) {
         if (!Bukkit.getPluginManager().isPluginEnabled("WorldGuard")) return false;
 
-        final RegionManager regionManager = WorldGuard.getInstance().getPlatform().getRegionContainer().get(BukkitAdapter.adapt(location.getWorld()));
+        final World world = location.getWorld();
+        final RegionManager regionManager = WorldGuard.getInstance().getPlatform().getRegionContainer().get(BukkitAdapter.adapt(world));
         if (regionManager == null) return false;
 
-        final Set<ProtectedRegion> protectedRegion = regionManager.getApplicableRegions(BukkitAdapter.asBlockVector(location)).getRegions();
-        return !protectedRegion.isEmpty() || Config.get().getBlacklistedWorlds().stream().anyMatch(world -> world.equalsIgnoreCase(location.getWorld().getName()));
+        final Chunk chunk = location.getChunk();
+        final BlockVector3 min = BukkitAdapter.asBlockVector(chunk.getBlock(0, world.getMinHeight(), 0).getLocation());
+        final BlockVector3 max = BukkitAdapter.asBlockVector(chunk.getBlock(15, world.getMaxHeight(), 15).getLocation());
+
+        final Set<ProtectedRegion> protectedRegion = regionManager.getApplicableRegions(new ProtectedCuboidRegion("tmp", min, max)).getRegions();
+        return !protectedRegion.isEmpty() || Config.get().getBlacklistedWorlds().stream().anyMatch(it -> it.equalsIgnoreCase(world.getName()));
     }
 }
